2bf6297efec07913e4cf2351f1fef4c8c62aa17e
kamil.wrobel <kamil.wrobel@zen.com>
2021-09-22 08:38:46 +0200

    add locks

--- server.js
+++ server.js
@@ -18,16 +18,23 @@ app.get('/:room', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 })
 
-
 io.on('connection', (socket) => {
   const room = socket.handshake.query.room;
+  let lockId = null;
+
   socket.join(room);
   console.log(`user ${socket.id} joined room ${room}`);
   socket.emit('content_updated', roomContent[room] || '');
 
+  socket.on('grant-lock', () => {
+    lockId = socket.id;
+    io.in(room).emit('lock-granted', lockId);
+    console.log(`room ${room} lock granted to ${lockId}`)
+  });
+
   socket.on('update_content', (newContent) => {
     const oldContent = roomContent[room];
-    if (newContent !== oldContent) {
+    if (newContent !== oldContent && lockId === socket.id) {
       console.log(`content updated by ${socket.id} in room ${room}`);
       socket.to(room).emit('content_updated', newContent);
       roomContent[room] = newContent;
